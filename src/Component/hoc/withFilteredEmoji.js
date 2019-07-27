2941a35b3149a889a5ed61262f0bc2d1ff6d8214
Nalhin <45081785+Nalhin@users.noreply.github.com>
2019-07-27 19:38:59 +0200

    Update withFilteredEmoji.js

--- src/Component/hoc/withFilteredEmoji.js
+++ src/Component/hoc/withFilteredEmoji.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
 export const withFilteredEmoji = Component => props => {
- //to be fixed
     return (
         <Component
             name={props.name}
             addRecentlyUsedEmoji={props.addRecentlyUsedEmoji}
             setCopyEmoji={props.setCopyEmoji}
-
             emoji={props.skinChange ? props.emoji.slice(0, 2) + props.skin.unicode + props.emoji.slice(2) : props.emoji}
         />
     );
